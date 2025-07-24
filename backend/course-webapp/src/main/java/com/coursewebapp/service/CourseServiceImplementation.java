26aad47e34ff5de198b6a4ab911f78196062f3e1
ALOKKUMARSAW <aloksaw512@gmail.com>
2025-07-24 12:18:30 +0530

    update files

--- backend/course-webapp/src/main/java/com/coursewebapp/service/CourseServiceImplementation.java
+++ backend/course-webapp/src/main/java/com/coursewebapp/service/CourseServiceImplementation.java
@@ -1,13 +1,12 @@
 package com.coursewebapp.service;
 
-import com.coursewebapp.dao.CourseDAO;
-import com.coursewebapp.entities.Course;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.coursewebapp.dao.CourseDAO;
+import com.coursewebapp.entities.Course;
 
 @Service
 public class CourseServiceImplementation implements CourseService {
@@ -21,11 +20,7 @@ public class CourseServiceImplementation implements CourseService {
     // Creating a list inside the default constructor.
     public  CourseServiceImplementation() {
 //        list = new ArrayList<>();
-//
-//        list.add(new Course(223,"Computer Networks","Build your Networking Fundamentals From Stratch."));
-//        list.add(new Course(521,"Cloud Computing","Start leveraging your Physical Resources- PAYG."));
-//        list.add(new Course(624,"System Design","Construct a robust system which serves millions."));
-//        list.add(new Course(992,"Data Structures & Algorithms","Organize & manage your data efficiently"));
+
     }
 
 // To get all courses.
@@ -37,15 +32,7 @@ public class CourseServiceImplementation implements CourseService {
     // To get single course using course ID.
     @Override
     public Course getCourse(long courseId) {
-//        Course c = null;
-//        for(Course course:list)
-//        {
-//            if(course.getId() == courseId)
-//            {
-//                c = course;
-//                break;
-//            }
-//        }
+//     
         return courseDAO.getReferenceById(courseId);
     }
 
@@ -60,15 +47,7 @@ public class CourseServiceImplementation implements CourseService {
     // Update Course
     @Override
     public Course updateCourse(Course course) {
-//        list.forEach(e -> {
-//            if (e.getId() == course.getId()) {
-//                e.setTitle(course.getTitle());
-//                e.setDescription(course.getDescription());
-//            }
-//
-//        });
-//        courseDAO.save(course);
- //       return course;
+//        
 
         // Handling corner case, if the course you're trying to update doesn't exist in the DB, it will throw error.
         if(courseDAO.existsById(course.getId())) {
@@ -82,7 +61,7 @@ public class CourseServiceImplementation implements CourseService {
     // Delete Course.
     @Override
     public void deleteCourse(long parseLong) {
-     //   list = this.list.stream().filter(e->e.getId() != parseLong).collect(Collectors.toList());
+
      Course entity = courseDAO.getReferenceById(parseLong);
      courseDAO.delete(entity);
     }
